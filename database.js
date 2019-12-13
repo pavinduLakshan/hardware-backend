8b942549e374d18ed1ffbb5d1b7658dcf87bb04f
pavinduLakshan <174084F@uom.lk>
2019-12-13 13:26:08 +0530

    mmmm

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+   
 const MONGODB_URL=process.env.NODE_ENV === "production" ? process.env.PROD_MONGODB_URL : process.env.MONGODB_URL 
 
 mongoose.connect(MONGODB_URL, {
