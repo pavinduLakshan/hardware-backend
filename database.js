ce17067679f70b100878412b45c6c945f8f404fb
pavinduLakshan <174084F@uom.lk>
2021-03-06 23:07:28 +0530

    change mongodb url

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
    
-const MONGODB_URL=process.env.NODE_ENV === "production" ? process.env.MONGODB_URI : process.env.MONGODB_URL 
+const MONGODB_URL=process.env.PROD_MONGODB_URL
 
 mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
